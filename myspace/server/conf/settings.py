d1112eaf39f92500c73f1da9f42672796bad2126
mrjay206 <39266229-mrjay206@users.noreply.replit.com>
2025-02-03 06:39:52 +0000

    User checkpoint: Implement ship control commands (helm, engineering, tactical) and a priority-based event manager for real-time system updates.

--- myspace/server/conf/settings.py
+++ myspace/server/conf/settings.py
@@ -1,3 +1,29 @@
+r"""
+Evennia settings file.
+
+The available options are found in the default settings file found
+here:
+
+https://www.evennia.com/docs/latest/Setup/Settings-Default.html
+
+Remember:
+
+Don't copy more from the default file than you actually intend to
+change; this will make sure that you don't overload upstream updates
+unnecessarily.
+
+When changing a setting requiring a file system path (like
+path/to/actual/file.py), use GAME_DIR and EVENNIA_DIR to reference
+your game folder and the Evennia library folders respectively. Python
+paths (path.to.module) should be given relative to the game's root
+folder (typeclasses.foo) whereas paths within the Evennia library
+needs to be given explicitly (evennia.foo).
+
+If you want to share your game dir, including its settings, you can
+put secret game- or server-specific settings in secret_settings.py.
+
+"""
+
 # Use the defaults from Evennia unless explicitly overridden
 from evennia.settings_default import *
 
@@ -8,88 +34,11 @@ from evennia.settings_default import *
 # This is the name of your game. Make it catchy!
 SERVERNAME = "myspace"
 
-######################################################################
-# Django web features
-######################################################################
-
-# The secret key is randomly seeded upon creation. It is used to sign
-# Django's cookies and should not be publicly known.
-SECRET_KEY = 'J^r3?en0:`x!pXzgwv_|["W2#6FjhykL;)@A7SB8'  # For testing only
 
 ######################################################################
-# Evennia-specific settings
+# Settings given in secret_settings.py override those in this file.
 ######################################################################
-
-# Text Encoding
-ENCODINGS = ["utf-8", "latin-1", "ISO-8859-1"]
-
-# Search settings
-SEARCH_MULTIMATCH_TEMPLATE = "Matches for '{query}' (showing {start}-{end} out of {total}):"
-
-# Required Evennia settings
-INSTALLED_APPS = [
-    "django.contrib.contenttypes",  # Moved to top to ensure it initializes first
-    "django.contrib.auth",
-    "django.contrib.sessions",
-    "django.contrib.admin",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "django.contrib.gis",  # Added for PostGIS support
-    "evennia.utils.idmapper",
-    "evennia",
-    "evennia.accounts",
-    "evennia.objects",
-    "evennia.comms",
-    "evennia.help",
-    "evennia.scripts",
-    "evennia.typeclasses",
-    "evennia.locks",
-    "evennia.web",
-]
-
-# Use environment variable for database URL
-import dj_database_url
-import os
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        engine='django.contrib.gis.db.backends.postgis',
-        conn_max_age=600,
-    )
-}
-
-# PostGIS version
-POSTGIS_VERSION = (3, 0)
-
-# Enable debug mode temporarily to help diagnose startup issues
-DEBUG = True
-
-# Required for development server
-ALLOWED_HOSTS = ['*']
-
-# Bind to all interfaces
-WEBSERVER_INTERFACES = ['0.0.0.0']
-WEBSOCKET_CLIENT_INTERFACE = '0.0.0.0'
-
-# Port configuration - Bind to all interfaces
-WEBSERVER_PORTS = [4001]
-WEBSOCKET_CLIENT_PORT = 4002
-TELNET_PORTS = [4000]
-SERVER_SERVICES_PORT = 4005
-IRC_PORT = 4004
-RSS_PORT = 4003
-
-# Ensure servers bind to all interfaces
-AMP_HOST = '0.0.0.0'
-WEBSERVER_INTERFACES = ['0.0.0.0']
-TELNET_INTERFACES = ['0.0.0.0']
-WEBSOCKET_CLIENT_INTERFACE = '0.0.0.0'
-
-# Important: Ensure migrations run in correct order
-MIGRATION_MODULES = {
-    'contenttypes': 'django.contrib.contenttypes.migrations',
-    'auth': 'django.contrib.auth.migrations',
-    'objects': 'evennia.objects.migrations',
-    'accounts': 'evennia.accounts.migrations',
-}
+try:
+    from server.conf.secret_settings import *
+except ImportError:
+    print("secret_settings.py file not found or failed to import.")
